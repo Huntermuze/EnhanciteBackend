1200163202ae4bef059315014453861c8fab27eb
huntermuze <mbyamen@gmail.com>
2023-10-10 23:28:42 +1100

    define datastream service api

---
+++ src/main/java/com/huntermuze/enhancite/service/DataStreamService.java
@@ -0,0 +1,20 @@
+package com.huntermuze.enhancite.service;
+
+import com.huntermuze.enhancite.dto.HeartRate;
+import com.huntermuze.enhancite.dto.Temperature;
+import com.huntermuze.enhancite.dto.container.HeartRates;
+import com.huntermuze.enhancite.dto.container.Temperatures;
+import com.huntermuze.enhancite.exception.NoDataFoundException;
+import com.huntermuze.enhancite.exception.UserNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface DataStreamService {
+    HeartRate getLatestHROfPatient(long patientId) throws UserNotFoundException, NoDataFoundException;
+
+    HeartRates getAllHRHistoryOfPatient(long patientId) throws UserNotFoundException, NoDataFoundException;
+
+    Temperature getLatestT1OfPatient(long patientId) throws UserNotFoundException, NoDataFoundException;
+
+    Temperatures getAllT1HistoryOfPatient(long patientId) throws UserNotFoundException, NoDataFoundException;
+}
