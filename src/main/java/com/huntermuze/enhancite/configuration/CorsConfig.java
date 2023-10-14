551beb0ed634acb5cd53235b4158a8ff87c8e991
huntermuze <mbyamen@gmail.com>
2023-10-14 21:41:29 +1100

    configure cors globally

---
+++ src/main/java/com/huntermuze/enhancite/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.huntermuze.enhancite.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nullable CorsRegistry registry) {
+                assert registry != null;
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
